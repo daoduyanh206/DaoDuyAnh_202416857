6275a0b970e18366749aec4619fa7db6be6176a3
daoduyanh206 <daoduyanh21112k6@gmail.com>
2026-04-06 12:42:41 +0700

    Add files via upload

---
+++ Cart.java
@@ -0,0 +1,42 @@
+public class Cart {
+    public static final int MAX_NUMBERS_ORDERED = 20;
+    private DigitalVideoDisc itemsOrdered[] = new DigitalVideoDisc[MAX_NUMBERS_ORDERED];
+
+    private int qtyOrdered = 0;
+
+    public void addDigitalVideoDisc(DigitalVideoDisc disc){
+        if(qtyOrdered >= MAX_NUMBERS_ORDERED)
+        {
+            System.out.println("The cart is already full");
+            return;
+        }
+        itemsOrdered[qtyOrdered] = disc;
+        qtyOrdered++;
+        System.out.println("The disc has been added");
+        if(qtyOrdered == MAX_NUMBERS_ORDERED) System.out.println("The cart is almost full");
+    }
+    public void removeDigitalVideoDisc(DigitalVideoDisc disc){
+        for (int i = 0; i < qtyOrdered; i++)
+        {
+            if(itemsOrdered[i] == disc)
+            {
+                for(int j = i + 1; j < qtyOrdered; j++)
+                    itemsOrdered[j-1] = itemsOrdered[j];
+                qtyOrdered--;
+                System.out.println("The disc has been removed");
+                return;
+            }
+        }
+        System.out.println("The disc is not in the cart");
+    }
+
+    public float totalCost()
+    {
+        float total = 0;
+        for (int i = 0; i < qtyOrdered; i++)
+        {
+            total += itemsOrdered[i].getCost();
+        }
+        return total;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# LuongDucAnh-202416853
