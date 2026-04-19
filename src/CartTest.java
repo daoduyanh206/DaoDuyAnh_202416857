f00ce9ce765cf8c80c900882fc894b0d6dfb82c7
Duy Anh <duyanh@MacBook-Pro-cua-Duy-Anh.local>
2026-04-19 19:48:26 +0700

    Add toString to DVD and print method to Cart

---
+++ src/CartTest.java
@@ -0,0 +1,19 @@
+public class CartTest {
+    public static void main(String[] args) {
+        // Tạo giỏ hàng mới
+        Cart cart = new Cart();
+
+        // Tạo các DVD và thêm vào giỏ
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // Kiểm tra phương thức in
+        cart.print();
+    }
+}
