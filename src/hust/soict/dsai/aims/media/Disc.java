1845d901184394d265fb1bebca40d4eac281e085
Duy Anh <duyanh@MacBook-Pro-cua-Duy-Anh.local>
2026-05-17 20:20:05 +0700

    Implement Disc, Track, and CompactDisc classes

---
+++ src/hust/soict/dsai/aims/media/Disc.java
@@ -0,0 +1,19 @@
+package hust.soict.dsai.aims.media;
+
+public class Disc extends Media {
+    private String director;
+    private int length;
+
+    public Disc() {
+        super();
+    }
+
+    public Disc(int id, String title, String category, float cost, String director, int length) {
+        super(id, title, category, cost);
+        this.director = director;
+        this.length = length;
+    }
+
+    public String getDirector() { return director; }
+    public int getLength() { return length; }
+}
